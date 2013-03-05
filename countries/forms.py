1f5ccea9f8f42df6c40e55a475c6751ce653dadd
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2013-03-05 14:42:54 -0500

    Adds more Haystack views

--- countries/forms.py
+++ countries/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from haystack.forms import SearchForm
 from .models import Country
 
 
@@ -9,5 +9,37 @@ GOVERNMENT_CHOICES = [('', '')] + [
 
 
 class CountrySearchForm(forms.Form):
+    """
+    Basic form for searching with QuerySet filter method
+    """
     q = forms.CharField(required=False)
     government = forms.ChoiceField(required=False, choices=GOVERNMENT_CHOICES)
+
+
+class CountryHaystackForm(SearchForm):
+    """
+    Slightly customized search form that allows filtering on the SearchQuerySet
+    """
+    government = forms.ChoiceField(required=False, choices=GOVERNMENT_CHOICES)
+
+    def search(self):
+        if not self.is_valid():
+            return self.no_query_found()
+
+        query = self.cleaned_data.get('q', '')
+        government = self.cleaned_data.get('government', '')
+        sqs = self.searchqueryset
+
+        if not query and not government:
+            return self.no_query_found()
+
+        if query:
+            sqs = sqs.auto_query(query)
+
+        if government:
+            sqs = sqs.filter(government=government)
+
+        if self.load_all:
+            sqs = sqs.load_all()
+
+        return sqs
--- factbook/urls.py
+++ factbook/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from haystack.views import SearchView, search_view_factory
+from haystack.forms import ModelSearchForm
+from haystack.query import SearchQuerySet
+
+from countries.forms import CountryHaystackForm
+
 
 admin.autodiscover()
 
@@ -9,9 +15,16 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', view='countries.views.country_list', name='index'),
     url(r'countries/', include('countries.urls')),
-    url(r'^haystack_default/', include('haystack.urls')),
+    url(r'^haystack/default/$', include('haystack.urls')),
+    url(r'^haystack/basic/$', search_view_factory(
+        view_class=SearchView,
+        form_class=ModelSearchForm
+    ), name='basic_search'),
+    url(r'^haystack/custom/$', search_view_factory(
+        view_class=SearchView,
+        form_class=CountryHaystackForm
+    ), name='custom_search'),
 )
-
 
 urlpatterns += staticfiles_urlpatterns()
 
