0059a9003202999dec0daf660daff986b173cde1
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2013-09-02 12:28:31 -0500

    Adds debug toolbar

--- factbook/settings.py
+++ factbook/settings.py
@@ -144,6 +144,15 @@ except ImportError:
 else:
     INSTALLED_APPS += ('django_extensions',)
 
+try:
+    import debug_toolbar
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS += ('debug_toolbar', 'haystack_panel')
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INTERNAL_IPS = ('127.0.0.1',)
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
@@ -191,3 +200,15 @@ ALLOWED_HOSTS = ['localhost', 'ciafactbook.herokuapp.com']
 RAVEN_CONFIG = {
     'dsn': env.get('DSN', ''),
 }
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'haystack_panel.panel.HaystackDebugPanel',
+)
