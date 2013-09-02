1ea8d366b387cdbdb3b98ddf9a176db29f46a864
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2013-09-02 12:41:16 -0500

    Index from template
    Shows how this is done and allows for more searchable text in examples

--- countries/search_indexes.py
+++ countries/search_indexes.py
@@ -1,16 +1,16 @@
 from haystack import indexes
-from django.template.defaultfilters import safe
 
 from .models import Country
 
 
 class CountryIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, model_attr='background')
+    text = indexes.CharField(document=True, use_template=True,
+            template_name="search/indexes/countries/country_text.txt")
     name = indexes.CharField(model_attr='name')
-    sort_name = indexes.CharField(model_attr='name', indexed=False, stored=True)
+    sort_name = indexes.CharField(model_attr='name', indexed=False,
+            stored=True)
     government = indexes.CharField(model_attr='government_type', faceted=True)
     population = indexes.IntegerField(model_attr='population')
 
     def get_model(self):
         return Country
-
